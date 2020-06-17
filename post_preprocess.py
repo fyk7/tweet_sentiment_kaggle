be9f0224cf58684872d05073b398dda2f140a2fc
fyk7 <tfscofield115@gmail.com>
2020-06-18 06:43:29 +0900

    rename files

--- post_preprocess.py
+++
@@ -1,2 +0,0 @@
-test['selected_text'] = test.apply(lambda x: post_process(x['selected_text']) if (len(str(x['selected_text']).split())==1) else x['selected_text'], axis=1)
-test['Jaccard'] = test.apply(lambda x: jaccard(str(x.selected_text), str(x.selected_text)), axis=1)
